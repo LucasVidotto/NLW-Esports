bc64b8840d3bf5016245de4cb2812fd1138315e4
Lucas Vidotto <lucasvidotto3@gmail.com>
2022-09-13 21:28:04 -0300

    mais um pouco da aula02

--- web/guia.md
+++ web/guia.md
@@ -4,4 +4,6 @@ w-1 ->width;
 
 lg: -> se ele estiver em telas grande, roda essa especificação(lg:bg-violet-500)(sm,md, lg);
 
-dark: -> se estiver no modo dark, muda a cor para essa (dark:bg-slate-500)
+dark: -> se estiver no modo dark, muda a cor para essa (dark:bg-slate-500);
+
+mx-auto -> margin no eixo x;
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,78 @@
 import './styles/input.css'
-
+import LogoImg from './assets/Logo.svg';
 function App() {
 
 
   return (
-    <div className="w-8 h-8 bg-black dark:bg-slate-500">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={LogoImg} alt="" />
+      <h1 className='text-6xl text-white font-black mt-20'> Seu
+        <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo</span>
+        está aqui! </h1>
 
-    </div> 
+      <div className='grid grid-cols-6 gap-6 mt-16' >
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/gm1.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Leagends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+
+        </a>
+        <a href="" className='relative'>
+          <img src="/gm2.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CS GO</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/gm3.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/gm4.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/gm5.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortinite</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/gm6.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+
+
+
+
+
+
+      </div>
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6  '>
+          <div>
+            <strong className='text-2xl text-white font-black black'>Não encontrou seu duo ?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
