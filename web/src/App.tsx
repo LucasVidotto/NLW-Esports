4981d5c6a8f77f624477947634d1d8717df3fce3
Lucas Vidotto <lucasvidotto3@gmail.com>
2022-09-14 09:01:24 -0300

    estilização pronta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/input.css'
 import LogoImg from './assets/Logo.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 function App() {
 
 
@@ -62,12 +63,13 @@ function App() {
 
       </div>
       <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6  '>
+        <div className='bg-[#2A2634] px-8 py-6  flex justify-between items-center'>
           <div>
             <strong className='text-2xl text-white font-black black'>Não encontrou seu duo ?</strong>
             <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
           </div>
-          <button>
+          <button className="py-3 px-3 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
             Publicar anúncio
           </button>
         </div>
